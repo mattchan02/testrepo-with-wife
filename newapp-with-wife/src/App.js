5cd97c42f09b385d1a29a4506a14e35cfd83fa6e
Erica-work <134347298+Erica-work@users.noreply.github.com>
2023-05-29 20:30:40 -0400

    Update App.js

--- newapp-with-wife/src/App.js
+++ newapp-with-wife/src/App.js
@@ -7,8 +7,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello Matthew<br></br>
-          <b>I LOVE you</b>
+          Hello Matthew Chan<br></br>
+          <u>I LOVE you</u>
         </p>
         <a
           className="App-link"
